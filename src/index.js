6d8b2d17bfd971098698275fd191c2d9ca70f8b2
maxx3322 <maxdgeers@gmail.com>
2020-01-14 10:35:17 +0800

    finished
    finsied work for practice of map, filter, reduce

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+import emojipedia from "./emojipedia";
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
+/*function double(x){
+  return (
+    x * 3
+  );
+}
+
+const newNumbers = numbers.map(double); 
+console.log(newNumbers)
 //Filter - Create a new array by keeping the items that return true.
+
+
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
+*/
+
+const newEmojipedia = emojipedia.map(function(y) {
+  return y.meaning.substring(0, 100);
+});
+
+console.log(newEmojipedia);
